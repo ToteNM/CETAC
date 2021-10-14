2f6510a5b8eb2ef7daf264d414e7737299109ed4
LeonardoM <A01652673@itesm.mx>
2021-10-14 16:44:32 -0700

    charts

---
+++ CETAC_Servicios 2/CETAC_Servicios/Controllers/Estadisticas/EstadisticasGlobalesViewController.swift
@@ -0,0 +1,31 @@
+//
+//  EstadisticasGlobalesViewController.swift
+//  CETAC_Servicios
+//
+//  Created by user193304 on 10/14/21.
+//
+
+import UIKit
+
+import TinyConstraints
+
+class EstadisticasGlobalesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
