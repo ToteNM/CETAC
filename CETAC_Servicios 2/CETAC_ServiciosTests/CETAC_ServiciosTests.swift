ab3c0938e5d0ffb17ba7929356b71e0fd2190725
LeonardoM <A01652673@itesm.mx>
2021-10-19 20:43:41 -0700

    a

---
+++ CETAC_Servicios 2/CETAC_ServiciosTests/CETAC_ServiciosTests.swift
@@ -0,0 +1,20 @@
+//
+//  CETAC_ServiciosTests.swift
+//  CETAC_ServiciosTests
+//
+//  Created by user193304 on 10/19/21.
+//
+
+import XCTest
+@testable import CETAC_Servicios
+import Firebase
+
+class CETAC_ServiciosTests: XCTestCase {
+
+    //var fetcher = fetcher
+    func testSesion() throws {
+        
+    }
+
+    
+}
